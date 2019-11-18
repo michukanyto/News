0f72d8fdc3d1968d5017e2a1de44b9a7b45b3f91
Marlon <escobar.marlon@gmail.com>
2019-11-18 12:56:49 -0500

    DownloadTask class implemented

---
+++ app/src/main/java/com/appsmontreal/news/api/DownloadTask.java
@@ -0,0 +1,47 @@
+package com.appsmontreal.news.api;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class DownloadTask extends AsyncTask <String, Void, String> {
+
+    private String result;
+    private URL url;
+    private HttpURLConnection urlConnection;
+
+
+    @Override
+    protected String doInBackground(String... urls) {
+        result = "";
+        urlConnection = null;
+
+        try {
+            url = new URL(urls[0]);
+            urlConnection = (HttpURLConnection) url.openConnection();
+            InputStream inputStream = urlConnection.getInputStream();
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+
+            int data = inputStreamReader.read();
+
+            while (data != -1) {
+                char current = (char) data;
+                result += current;
+                data = inputStreamReader.read();
+                Log.i("Log URL CONTENT ====> ", result);
+                return result;
+
+            }
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+}
--- app/src/main/java/com/appsmontreal/news/view/MainActivity.java
+++ app/src/main/java/com/appsmontreal/news/view/MainActivity.java
@@ -1,8 +1,10 @@
-package com.appsmontreal.news;
+package com.appsmontreal.news.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.appsmontreal.news.R;
 
 public class MainActivity extends AppCompatActivity {
 
